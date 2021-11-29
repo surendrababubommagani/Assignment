c26ecd6c0869d6d407d12e5e9ae4dc420c6714a0
surendrababubommagani <94668323+surendrababubommagani@users.noreply.github.com>
2021-11-29 11:55:29 +0530

    Add files via upload

---
+++ Spring Rest ass1/SpringRestAssignment-4/src/main/java/com/surendra/SpringRestAssignment4Application.java
@@ -0,0 +1,13 @@
+package com.surendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAssignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAssignment4Application.class, args);
+	}
+
+}
